ae06afdcf2ba5af027306513a80d790b9e03feb2
Santak Mishra <santakmishra@outlook.com>
2016-12-15 20:00:22 -0600

    Updated sql, removed Create DB command

--- api/includes/sqlfile/recipemanager.sql
+++ api/includes/sqlfile/recipemanager.sql
@@ -1,32 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Dec 14, 2016 at 06:08 AM
--- Server version: 5.6.33
--- PHP Version: 7.0.0
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
---
--- Database: `recipemanager`
---
-CREATE DATABASE IF NOT EXISTS `recipemanager` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `recipemanager`;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
 
 CREATE TABLE `users` (
   `user_id` int(11) NOT NULL,
@@ -36,23 +15,11 @@ CREATE TABLE `users` (
   `user_status` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `users`
---
 ALTER TABLE `users`
   MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
