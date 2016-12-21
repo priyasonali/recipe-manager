410f5e90741e2adec53f5b21b9c2544074d41b2f
Sonali Priya <sonalipriya@outlook.com>
2016-12-20 20:04:11 -0600

    Implemented forgot password

--- api/forgotpass.php
+++ api/forgotpass.php
@@ -6,10 +6,9 @@
 
     class ForgotPass{
 
-        public function __construct($uemail, $upass) {
+        public function __construct($uemail) {
         
-            $this->userEmail = $uemail;
-            $this->userPass = $upass;
+            $this->userEmail = $uemail['user_email'];
 
         }
 
@@ -18,33 +17,51 @@
             $mysqli = $db->connect();
             $errnum = new ErrorList;
    
-            if($this->userEmail != null && $this->userPass != null){
+            if($this->userEmail != null){
 
                 $result = $mysqli->query("SELECT * FROM users WHERE user_email = '$this->userEmail'");
                 $row = $result->fetch_assoc();
                 $user_email = $row['user_email'];
+                $user_name = $row['user_name'];
 
                 if($user_email == $this->userEmail){
-  
-                    $result = $mysqli->query("UPDATE users SET user_pass='$this->userPass' WHERE user_email='$this->userEmail'");
-                    if($result===TRUE){
-                        $response["status"] = "Success"; 
-                        
-                    }  
+                    $tem_pass = rand(111111, 99999999);
+                    $to = $this->user_email;
+                    $subject = "New password";
+                    $txt = "
+                            <html>
+                                <head>
+                                    <title>Recipe Manager</title>
+                                </head>
+                                <body>
+                                    <p>Following is your new password and email: </p>
+                                    <br>
+                                    <b>Username: </b> $user_name <br>
+                                    <b>Password:</b> $tem_pass<br>
+                                    <b>Thank You</b>
+                                </body>
+                            </html>
+                            ";
+                    $headers = "From: noreply@recipemanager.com" . "\r\n" .
+                    mail($to,$subject,$txt,$headers); 
+                    if($mysqli->query("UPDATE users SET user_pass='$tem_pass' WHERE user_email = '$this->userEmail'")){
+                    $response["status"] = "success";
+                    }
                     else{
-                        
-                    $response["status"] = "Failed";
+                    $response["status"] = "failure";
                     $response["error"]["err_code"] = 5;
                     $response["error"]["err_desc"] = $errnum->errlist[5];
-                    } 
+
+                    }
                 }
                 else{
-                    $response["status"] = "Failed";
+                    $response["status"] = "failure";
                     $response["error"]["err_code"] = 4;
                     $response["error"]["err_desc"] = $errnum->errlist[4];
                 }
                 
             } else {
+                $response["status"] = "failure";
                 $response["error"]["err_code"] = 2;
                 $response["error"]["err_desc"] = $errnum->errlist[2];
             }
@@ -54,7 +71,7 @@
 
 
     }
-    $check = new ForgotPass($_REQUEST["user_email"],$_REQUEST["user_pass"], $uid);
+    $check = new ForgotPass($request);
     echo $check->forgot();
 
 ?>
