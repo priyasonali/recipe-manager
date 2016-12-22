29d870c15b972bd6ab87fef7d7e9d00d1bf932d6
Santak Mishra <santakmishra@outlook.com>
2016-12-21 19:58:47 -0600

    Forgot password implemented

--- scripts/controllers/LoginController.js
+++ scripts/controllers/LoginController.js
@@ -4,6 +4,10 @@ angular.module('RecipeManager')
         function($scope, $timeout, $location, api) {
             var ctrl = this;
 
+            ctrl.credentials = {};
+            ctrl.forgotten = false;
+            ctrl.fgtpass = {};
+
             ctrl.uNameRegX = /^[a-z]+$/;
             ctrl.uPassRegX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!=@#$%^&-])[A-Za-z\d!=@#$%^&-]+$/;
             ctrl.errorChk = function(element, error){
@@ -16,10 +20,24 @@ angular.module('RecipeManager')
                 }
             };
 
+            ctrl.emailChk = function(element, error){
+                var form = $scope.forgotForm;
+                var formElement = $scope.forgotForm[element];
+                if(Array.prototype.slice.call(arguments).length == 1) {
+                    return (formElement.$touched || form.$submitted) && formElement.$invalid;
+                } else {
+                    return (formElement.$touched || form.$submitted) && formElement.$error[error];
+                }
+            };
+
             ctrl.login = function(credentials) {
                 credentials.action = 'signin';
                 api().fetch(credentials).$promise.then(function(response){
                     ctrl.response = response;
+                    if(response.status == "success") {
+                        ctrl.successMsg = "Authentication Successful! Redirecting to Dashboard..."; 
+                    }
+                    
                     if(response.token) {
                         sessionStorage.setItem("authToken", response.token);
                         $timeout(function(){
@@ -38,5 +56,31 @@ angular.module('RecipeManager')
                     };
                 });
             };
+
+            ctrl.forgotPass = function() {
+                ctrl.forgotten = !ctrl.forgotten;
+                ctrl.response = {};
+            };
+
+            ctrl.fetchPass = function() {
+                ctrl.fgtpass.action = "forgotpass";
+                api().fetch(ctrl.fgtpass).$promise.then(function(response){
+                    ctrl.response = response;
+                    if(response.status == "success") {
+                        ctrl.forgotten = !ctrl.forgotten;
+                        ctrl.fgtpass = {};
+                        ctrl.successMsg = "Your username and a temporary password has been sent to your email address. Remember to change your password after logging in.";
+                    }
+                },function(response){
+                    ctrl.response = {
+                        status: 'failure',
+                        error: {
+                            err_code: response.status,
+                            err_desc: response.statusText
+                        }
+                    };
+                });
+            };
+
         }
     ]);
