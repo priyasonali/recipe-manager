3769589d3b00e455f1f49edfb133103e7a2e9b41
Sonali Priya <sonalipriya@outlook.com>
2016-12-21 21:12:10 -0600

    Updated sql file

--- api/install/rm.sql
+++ api/install/rm.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Dec 20, 2016 at 08:11 PM
+-- Generation Time: Dec 22, 2016 at 04:11 AM
 -- Server version: 5.6.33
 -- PHP Version: 7.0.12
 
@@ -23,10 +23,48 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `ingredients`
+--
+
+CREATE TABLE `ingredients` (
+  `ing_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `ing_name` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `recipe`
+--
+
+CREATE TABLE `recipe` (
+  `recp_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `recp_name` varchar(30) NOT NULL,
+  `recp_desc` varchar(20000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `recipe_ing`
+--
+
+CREATE TABLE `recipe_ing` (
+  `ri_id` int(11) NOT NULL,
+  `recp_id` int(50) NOT NULL,
+  `ing_id` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tmp_users`
 --
 
 CREATE TABLE `tmp_users` (
+  `tmp_id` int(11) NOT NULL,
   `tmp_email` varchar(50) NOT NULL,
   `tmp_code` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -49,6 +87,30 @@ CREATE TABLE `users` (
 --
 
 --
+-- Indexes for table `ingredients`
+--
+ALTER TABLE `ingredients`
+  ADD PRIMARY KEY (`ing_id`);
+
+--
+-- Indexes for table `recipe`
+--
+ALTER TABLE `recipe`
+  ADD PRIMARY KEY (`recp_id`);
+
+--
+-- Indexes for table `recipe_ing`
+--
+ALTER TABLE `recipe_ing`
+  ADD PRIMARY KEY (`ri_id`);
+
+--
+-- Indexes for table `tmp_users`
+--
+ALTER TABLE `tmp_users`
+  ADD PRIMARY KEY (`tmp_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -59,10 +121,30 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `ingredients`
+--
+ALTER TABLE `ingredients`
+  MODIFY `ing_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `recipe`
+--
+ALTER TABLE `recipe`
+  MODIFY `recp_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `recipe_ing`
+--
+ALTER TABLE `recipe_ing`
+  MODIFY `ri_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `tmp_users`
+--
+ALTER TABLE `tmp_users`
+  MODIFY `tmp_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
